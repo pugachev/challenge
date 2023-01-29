3b4210e1bb1765eef4997ce4f524d997308c172e
pugachev <pugachev2011@gmail.com>
2023-01-29 11:34:59 +0900

    微修正

--- j11.php
+++ j11.php
@@ -41,41 +41,94 @@ if(isset($_POST['csvtype']) && !empty($_POST['csvtype'])) {
   //そのまま全行
   else
   {
+    //ヘッダ書き込み
     $header = ['financialnumber','totalprice'];
     fputcsv($fp, $header);
-    $prenumber = '';
-    $totalprice = 0;
-    $sumdata = [];
+
 
     //financialnumberよりユニークキーを取り出す
     $keyarray = array_unique(array_column($results, "financialnumber"));
 
-    $groups;
-    foreach ($keyarray as $key) {
-      if(empty($groups)){
-        $groups = array($key => 0);
-      }else{
-        $groups = array_merge($groups,array($key => 0));
-      }
+    //加算用連想配列を作成しておく
+    foreach ($keyarray as $key){
+      $addUp[$key]=0;
     }
 
-    foreach($results as $result){
-      foreach ($keyarray as $key) {
+
+
+    //A001とA002がとりだされる この2つでまとめて足し込む
+    //DBから出力されるデータは以下のイメージ
+    // $results[] = array ( 'id' => '6', 'financialnumber' => 'A001', 'item' => 'カレー', 'price' => '1000', 'outputflg' => '1', );
+    // $results[] = array ( 'id' => '7', 'financialnumber' => 'A002', 'item' => 'ラーメン', 'price' => '800', 'outputflg' => '1', );
+    // $results[] = array ( 'id' => '8', 'financialnumber' => 'A001', 'item' => 'うどん', 'price' => '850', 'outputflg' => '1', );
+    // $results[] = array ( 'id' => '9', 'financialnumber' => 'A001', 'item' => '焼き飯', 'price' => '700', 'outputflg' => '1', );
+    // $results[] = array ( 'id' => '10', 'financialnumber' => 'A002', 'item' => 'パン', 'price' => '600', 'outputflg' => '1', );
+    foreach($keyarray as $key){
+      foreach($results as $result){
         if (in_array($key, $result, true)) {
-          $groups[$key] += $result['price'];
+          $addUp[$key] += $result['price'];
         }
       }
     }
 
-    //出力用の配列を生成する
-    $outputarray = [];
-    foreach($keyarray as $key){
-      $outputarray[] = [$key,$groups[$key]];
+    //CSV出力用の配列を作成する
+    foreach($addUp as $key=>$val){
+      $outputData[] = [$key,$val];
+      // var_dump($key.'   '.$val);
+    }
+    // foreach ($keyarray as $key) {
+    //   $outputData = array($key,$addUp[$key]);
+    // }
+
+    //CSVを出力する
+    foreach ($outputData as $value) {
+      // foreach($value as $key=>$val){
+        fputcsv($fp, array($value[0],$value[1]));
+        // echo $value[0] . '   ' . $value[1] . PHP_EOL;
+      // }
     }
 
-    foreach ($outputarray as $value) {
-      fputcsv($fp, $value);
-    }
+    // die();
+
+
+
+
+
+    // $header = ['financialnumber','totalprice'];
+    // fputcsv($fp, $header);
+    // $prenumber = '';
+    // $totalprice = 0;
+    // $sumdata = [];
+
+    // //financialnumberよりユニークキーを取り出す
+    // $keyarray = array_unique(array_column($results, "financialnumber"));
+
+    // $groups;
+    // foreach ($keyarray as $key) {
+    //   if(empty($groups)){
+    //     $groups = array($key => 0);
+    //   }else{
+    //     $groups = array_merge($groups,array($key => 0));
+    //   }
+    // }
+
+    // foreach($results as $result){
+    //   foreach ($keyarray as $key) {
+    //     if (in_array($key, $result, true)) {
+    //       $groups[$key] += $result['price'];
+    //     }
+    //   }
+    // }
+
+    // //出力用の配列を生成する
+    // $outputarray = [];
+    // foreach($keyarray as $key){
+    //   $outputarray[] = [$key,$groups[$key]];
+    // }
+
+    // foreach ($outputarray as $value) {
+    //   fputcsv($fp, $value);
+    // }
   }
 
   // ファイルを閉じる
