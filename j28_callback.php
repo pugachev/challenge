f936a24248ee552c977d524c860f93c220d8eac2
pugachev <pugachev2011@gmail.com>
2023-04-22 10:08:44 +0900

    一旦コミット

--- j28_callback.php
+++ j28_callback.php
@@ -2,49 +2,50 @@
 
 <?php
 // アプリケーション設定
-define('CLIENT_ID', '1076303706791-lo4j05trdq3q8d8cr5bjieasbmh9cn3n.apps.googleusercontent.com');
-define('CLIENT_SECRET', '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');
+// define('CLIENT_ID', '1076303706791-lo4j05trdq3q8d8cr5bjieasbmh9cn3n.apps.googleusercontent.com');
+// define('CLIENT_SECRET', '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');
 
-// エンドポイント
-define('TOKEN_URL', 'https://oauth2.googleapis.com/token'); // トークンエンドポイント
-define('CALLBACK_URL', 'https://challenge.ikefukuro40.tech/j28_callback.php'); // リダイレクトエンドポイント
+// // エンドポイント
+// define('TOKEN_URL', 'https://oauth2.googleapis.com/token'); // トークンエンドポイント
+// define('CALLBACK_URL', 'https://challenge.ikefukuro40.tech/j28_callback.php'); // リダイレクトエンドポイント
 
-// リソース
-define('ALBUMS_URL', 'https://photoslibrary.googleapis.com/v1/albums');
+// // リソース
+// define('ALBUMS_URL', 'https://photoslibrary.googleapis.com/v1/albums');
 
-$code = $_GET['code']; // 認可コード
-$params = array(
-  'code'          => $code,
-	'client_id'     => CLIENT_ID,
-	'client_secret' => CLIENT_SECRET,
-	'redirect_uri'  => CALLBACK_URL,
-	'grant_type'    => 'authorization_code'
-);
+// $code = $_GET['code']; // 認可コード
 
-// POST送信
-$options = array('http' => array(
-   "method"  => "POST",
-   "content" => http_build_query($params)
-));
+// $params = array(
+//   'code'          => $code,
+// 	'client_id'     => CLIENT_ID,
+// 	'client_secret' => CLIENT_SECRET,
+// 	'redirect_uri'  => CALLBACK_URL,
+// 	'grant_type'    => 'authorization_code'
+// );
 
-// アクセストークン情報一覧の取得
-$res = file_get_contents(TOKEN_URL, false, stream_context_create($options));
+// // POST送信
+// $options = array('http' => array(
+//    "method"  => "POST",
+//    "content" => http_build_query($params)
+// ));
 
-// アクセストークンの取得
-$token = json_decode($res, true);
-if(isset($token['error'])){
-	echo 'エラー発生';
-	exit;
-}
-$access_token = $token['access_token'];
+// // アクセストークン情報一覧の取得
+// $res = file_get_contents(TOKEN_URL, false, stream_context_create($options));
 
-$params = array('access_token' => $access_token);
+// // アクセストークンの取得
+// $token = json_decode($res, true);
+// if(isset($token['error'])){
+// 	echo 'エラー発生';
+// 	exit;
+// }
+// $access_token = $token['access_token'];
 
-// アルバム情報取得
-$res = file_get_contents(ALBUMS_URL . '?' . http_build_query($params));
+// $params = array('access_token' => $access_token);
 
-//表示
-echo $res;
+// // アルバム情報取得
+// $res = file_get_contents(ALBUMS_URL . '?' . http_build_query($params));
+
+// //表示
+// echo $res;
 
 
 ?>
